1a09682dd3513bdfeb4f3781928a8e90d91971bb
Stijn Goethals <stijn@tealpartners.com>
2019-08-06 22:02:05 +0200

    verwijder sancties

--- web/src/app/info/leefregels/leefregels.component.ts
+++ web/src/app/info/leefregels/leefregels.component.ts
@@ -1,40 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface SanctieElement {
-  wat: string;
-  oke: string;
-  sanctie: string;
-}
-
-const Sancties: SanctieElement[] = [
-  {
-    wat: '(alle) drugs', oke: 'NEEN',
-    sanctie: 'Naar huis + natraject'
-  },
-  {
-    wat: 'Dronken aankomen op HO', oke: 'Sanctie bij overlast met schade',
-    sanctie: 'Overlast: verwittigen ploeg veiligheid\nOverlast met schade: naar huis + natraject'
-  },
-  {
-    wat: 'Eigen drank', oke: 'NEEN',
-    sanctie: 'Indien weigering om drank af te geven: toegang tot HO ontzeggen / afnemen + ploeg veiligheid verwittigen'
-  },
-  {
-    wat: 'Pintjes / alcohol', oke: 'Sanctie bij overlast met schade',
-    sanctie: 'Overlast: verwittigen ploeg veiligheid\nOverlast met schade: naar huis + natraject'
-  }
-];
-
-
 @Component({
   selector: 'app-leefregels',
   templateUrl: './leefregels.component.html',
   styleUrls: ['./leefregels.component.scss']
 })
 export class LeefregelsComponent implements OnInit {
-
-  public displayedColumns: string[] = ['wat', 'oke', 'sanctie'];
-  public dataSource = Sancties;
 
   constructor() { }
 
