1de2686df1f052022057bf1d0fedbc7f896eb1aa
Stijn Goethals <stijn@tealpartners.com>
2019-08-16 22:36:42 +0200

    add scrollPositionRestoration

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -56,7 +56,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
